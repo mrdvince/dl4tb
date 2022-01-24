84abdea9c7d8d82cee515852d0c705537ef75a5a
Vince <vincivenv@gmail.com>
2022-01-24 18:38:20 +0300

    update data unzip path

--- get_data.py
+++ get_data.py
@@ -7,14 +7,15 @@ import gdown
 def get_data(url, path, filename):
     path = Path(path)
     path.mkdir(parents=True, exist_ok=True)
-    gdown.download(url, str(path / filename), quiet=False)
+    if not path.joinpath(filename).exists():
+        gdown.download(url, path.joinpath(filename).as_posix(), quiet=False)
     # extract the zip file
     unzip(path, filename)
 
 
 def unzip(path, filename):
     with zipfile.ZipFile(str(path / filename), "r") as zip_ref:
-        zip_ref.extractall(path)
+        zip_ref.extractall(path / filename.replace(".zip", ""))
 
 
 if __name__ == "__main__":
