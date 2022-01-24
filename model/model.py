01fb92488c8cd4ecc01a4ddac26f0800d3b5800d
Vince <vincivenv@gmail.com>
2022-01-24 16:03:16 +0300

    Models, densenet121 and 201

--- model/model.py
+++ model/model.py
@@ -1,23 +1,74 @@
-import torch
+from abc import abstractmethod
+
+import numpy as np
 import torch.nn as nn
-import torch.nn.functional as F
+import torchvision
 
 
-class Model(nn.Module):
-    def __init__(self, num_classes=10):
-        super().__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, num_classes)
+class BaseModel(nn.Module):
+    @abstractmethod
+    def forward(self, *inputs):
+        raise NotImplementedError
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        model_parameters = filter(lambda p: p.requires_grad, self.parameters())
+        params = sum([np.prod(p.size()) for p in model_parameters])
+        return super().__str__() + "\nTrainable parameters: {}".format(params)
+
+
+class Densenet201(BaseModel):
+    def __init__(self, num_classes):
+        super(Densenet201, self).__init__()
+        self.model = torchvision.models.densenet201(pretrained=False)
+        for param in self.model.parameters():
+            param.requires_grad = False
+
+        self.model.classifier = nn.Sequential(
+            nn.Linear(self.model.classifier.in_features, 512),
+            nn.Dropout(0.2),
+            nn.ReLU(),
+            nn.Linear(512, num_classes),
+        )
+        # weight initialization
+        for m in self.modules:
+            if isinstance(m, nn.Linear):
+                # using the range [−𝑦,𝑦] , where  𝑦=1/√𝑛 , 𝑛 is the number of inputs to a given neuron.
+                # get the number of the inputs
+                n = m.in_features
+                y = 1.0 / np.sqrt(n)
+                m.weight.data.uniform_(-y, y)
+                m.bias.data.fill_(0)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = torch.flatten(x, 1)  # flatten all dimensions except batch
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+        return self.model(x)
+
+
+class Densenet121(BaseModel):
+    def __init__(self, num_classes):
+        super(Densenet201, self).__init__()
+        self.model = torchvision.models.densenet121(pretrained=False)
+        for param in self.model.parameters():
+            param.requires_grad = False
+        # new definations have requires grad by default
+        self.model.classifier = nn.Sequential(
+            nn.Linear(self.model.classifier.in_features, 512),
+            nn.Dropout(0.2),
+            nn.ReLU(),
+            nn.Linear(512, num_classes),
+        )
+
+        # weight initialization
+        for m in self.modules:
+            if isinstance(m, nn.Linear):
+                # using the range [−𝑦,𝑦] , where  𝑦=1/√𝑛 , 𝑛 is the number of inputs to a given neuron.
+                # get the number of the inputs
+                n = m.in_features
+                y = 1.0 / np.sqrt(n)
+                m.weight.data.uniform_(-y, y)
+                m.bias.data.fill_(0)
+
+    def forward(self, x):
+        return self.model(x)
