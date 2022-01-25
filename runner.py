f5f4a2adca03229aebd17443156d4a36a769b68a
Vince <vincivenv@gmail.com>
2022-01-25 16:46:57 +0300

    chore: Format

--- runner.py
+++ runner.py
@@ -40,35 +40,44 @@ def get_device(config):
 
     return torch.device(device)
 
+
 def permute_params(model, to_filters_last, lazy_mode):
     with torch.no_grad():
         for name, param in model.named_parameters():
-            if(param.ndim == 4):
+            if param.ndim == 4:
                 if to_filters_last:
-                    param.data = param.data.permute((2, 3, 1, 0))  # permute KCRS to RSCK 
+                    param.data = param.data.permute(
+                        (2, 3, 1, 0)
+                    )  # permute KCRS to RSCK
                 else:
-                    param.data = param.data.permute((3, 2, 0, 1))  # permute RSCK to KCRS
+                    param.data = param.data.permute(
+                        (3, 2, 0, 1)
+                    )  # permute RSCK to KCRS
     if lazy_mode:
         import habana_frameworks.torch.core as htcore
+
         htcore.mark_step()
+
 
 def permute_momentum(optimizer, to_filters_last, lazy_mode):
     # Permute the momentum buffer before using for checkpoint
     for group in optimizer.param_groups:
-        for p in group['params']:
+        for p in group["params"]:
             param_state = optimizer.state[p]
-            if 'momentum_buffer' in param_state:
-                buf = param_state['momentum_buffer']
-                if(buf.ndim == 4):
+            if "momentum_buffer" in param_state:
+                buf = param_state["momentum_buffer"]
+                if buf.ndim == 4:
                     if to_filters_last:
-                        buf = buf.permute((2,3,1,0))
+                        buf = buf.permute((2, 3, 1, 0))
                     else:
-                        buf = buf.permute((3,2,0,1))
-                    param_state['momentum_buffer'] = buf
+                        buf = buf.permute((3, 2, 0, 1))
+                    param_state["momentum_buffer"] = buf
 
     if lazy_mode:
         import habana_frameworks.torch.core as htcore
+
         htcore.mark_step()
+
 
 def main(config):
     # dataloaders
