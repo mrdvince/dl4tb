f8c2e848725fe4f533b3ee7e8be91d38f919e74b
Vince <vincivenv@gmail.com>
2022-02-04 13:31:02 +0300

    add container build steps

---
+++ Dockerfile
@@ -0,0 +1,32 @@
+from python:3.9-slim
+
+WORKDIR /app
+RUN pip install --upgrade pip
+RUN pip install torch==1.10.2+cpu torchvision==0.11.3+cpu  -f https://download.pytorch.org/whl/cpu/torch_stable.html
+
+COPY ./inf_requirements.txt /tmp/inf_requirements.txt
+
+RUN pip install -r /tmp/inf_requirements.txt
+
+RUN pip install "dvc[s3]"
+
+COPY . /app
+
+ARG AWS_ACCESS_KEY_ID
+ARG AWS_SECRET_ACCESS_KEY
+
+
+# aws credentials configuration
+ENV AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+    AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+
+
+RUN dvc init --no-scm
+# configuring remote server in dvc
+RUN dvc remote add -d storage s3://dvc-store123/trained_models
+# pulling the trained model
+RUN dvc pull dvcfiles/onnx_model.dvc
+
+EXPOSE 8000
+
+CMD ["uvicorn", "app:app", "--host", "0.0.0.0", "--port", "8000"]
---
+++ src/app.py
@@ -0,0 +1,34 @@
+from io import BytesIO
+from typing import Any
+
+from fastapi import FastAPI, File, UploadFile
+from PIL import Image
+from starlette.responses import RedirectResponse
+
+from inference import TchPredictor
+
+app = FastAPI(title="dl4tb")
+
+
+@app.get("/", tags=["redirect"])
+def redirect_to_docs() -> Any:
+    return RedirectResponse(url="redoc")
+
+
+# load the model
+predictor = TchPredictor("../saved/exports/model_best_checkpoint.onnx", onnx=True)
+
+
+def read_image_from_upload(upload_file: UploadFile):
+    img_stream = BytesIO(upload_file.file.read())
+    return Image.open(img_stream).convert("RGB")
+
+
+@app.post("/")
+def get_predictions(file: UploadFile = File(...)) -> Any:
+    """
+    Upload image and get prediction
+    """
+    img = read_image_from_upload(file)
+    pred = predictor.onnx_predict(img)
+    return pred
