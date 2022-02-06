1cf2d0d2881652c60865a95d179bb0ca747cd311
Vince <vincivenv@gmail.com>
2022-02-06 14:23:33 +0300

    fix: Update data dims

--- src/data.py
+++ src/data.py
@@ -35,7 +35,7 @@ class DataModule(pl.LightningDataModule):
         super(DataModule, self).__init__()
         self.transforms = transforms.Compose(
             [
-                transforms.RandomResizedCrop(2),
+                transforms.RandomResizedCrop(224),
                 transforms.ToTensor(),
                 transforms.Normalize(
                     mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
