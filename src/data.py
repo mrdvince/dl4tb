3de0c9c5c090b89dfad277bbdcc351744e395f9e
Vince <vincivenv@gmail.com>
2022-02-09 14:20:57 +0300

    unet data module

--- src/data.py
+++ src/data.py
@@ -1,19 +1,121 @@
+import os
 import zipfile
+from glob import glob
 from pathlib import Path
 from typing import Optional
 
+import albumentations as A
+import cv2
 import gdown
 import hydra
+import numpy as np
 import pytorch_lightning as pl
-from torch.utils.data import DataLoader, random_split
+import torch
+from skimage.io import imread as imread
+from skimage.transform import resize as resize
+from torch.utils.data import DataLoader, TensorDataset, random_split
 from torchvision import datasets, transforms
+from tqdm import tqdm
 
-from utils import copy_images_to_folder
+from utils import copy_images_to_folder, download
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 
-class DataModule(pl.LightningDataModule):
+class PROCData:
+    def __init__(self):
+        self.transform = A.Compose(
+            [
+                A.HorizontalFlip(p=0.5),
+                A.ShiftScaleRotate(rotate_limit=15, always_apply=True),
+            ]
+        )
+        image_paths = glob(
+            os.path.join(
+                "data/pulmonary-chest-xray-abnormalities/Montgomery/MontgomerySet/CXR_png",
+                "*.png",
+            )
+        )
+        # fmt: off
+        self.images_with_masks_paths = [
+            (image_path,os.path.join("/".join(image_path.split("/")[:-2]),"ManualMask","leftMask", os.path.basename(image_path)),
+                os.path.join("/".join(image_path.split("/")[:-2]),"ManualMask","rightMask",os.path.basename(image_path))) for image_path in image_paths
+            ]
+        # fmt: on
+        self.OUT_DIM = (512, 512)
+
+    def process(self):
+        images, masks = [], []
+        for mri, left_lung, right_lung in tqdm(
+            self.images_with_masks_paths, position=0, leave=True
+        ):
+            images.append(self.image_from_path(mri))
+            masks.append(self.mask_from_paths(left_lung, right_lung))
+
+        transformed_images, transformed_masks = [], []
+
+        for image, mask in zip(images, masks):
+            sample = {"image": image.copy(), "mask": mask.copy()}
+            out = self.transform(**sample)
+            transformed_images.append(out["image"])
+            transformed_masks.append(out["mask"])
+
+        image_dataset = images.copy() + transformed_images
+        mask_dataset = masks.copy() + transformed_masks
+
+        x_train, x_val, y_train, y_val = train_test_split(
+            image_dataset, mask_dataset, test_size=0.2
+        )
+        scaler = StandardScaler()
+        x_train = scaler.fit_transform(
+            np.array(x_train).reshape(-1, 512 * 512)
+        ).reshape(-1, 512, 512)
+        x_val = scaler.transform(np.array(x_val).reshape(-1, 512 * 512)).reshape(
+            -1, 512, 512
+        )
+        return x_train, y_train, x_val, y_val
+
+    def image_from_path(self, path):
+        img = resize(imread(path), self.OUT_DIM, mode="constant")
+        return img
+
+    def mask_from_paths(self, path1, path2):
+        img = resize(
+            cv2.bitwise_or(imread(path1), imread(path2)), self.OUT_DIM, mode="constant"
+        )
+        return img
+
+
+class UNETDataModule(pl.LightningDataModule):
+    def __init__(self, config=None):
+        super(UNETDataModule, self).__init__()
+        self.project_root = os.getcwd() + "/"  # hydra.utils.get_original_cwd() + "/"
+        print(self.project_root)
+
+    def prepare_data(self):
+        if not os.path.exists(
+            self.project_root + "data/" + "pulmonary-chest-xray-abnormalities"
+        ):
+            download(
+                "https://www.kaggle.com/kmader/pulmonary-chest-xray-abnormalities",
+                self.project_root + "data",
+            )
+
+    def setup(self, stage: Optional[str] = None):
+        x_train, y_train, x_val, y_val = PROCData().process()
+        self.train_ds = TensorDataset(torch.tensor(x_train), torch.tensor(y_train))
+        self.val_ds = TensorDataset(torch.tensor(x_val), torch.tensor(y_val))
+
+    def train_dataloader(self):
+        return DataLoader(self.train_ds, batch_size=32, shuffle=True, pin_memory=True)
+
+    def val_dataloader(self):
+        return DataLoader(self.val_ds, batch_size=32, shuffle=False)
+
+
+class ClassifierDataModule(pl.LightningDataModule):
     def __init__(self, config):
-        super(DataModule, self).__init__()
+        super(ClassifierDataModule, self).__init__()
         self.transforms = transforms.Compose(
             [
                 transforms.RandomResizedCrop(224),
@@ -81,3 +183,13 @@ class DataModule(pl.LightningDataModule):
 
     def test_dataloader(self):
         return DataLoader(self.test_data, shuffle=False, batch_size=32, num_workers=6)
+
+
+if __name__ == "__main__":
+    train_loader = UNETDataModule()
+    train_loader.prepare_data()
+    train_loader.setup()
+    train_loader = train_loader.train_dataloader()
+    data = next(iter(train_loader))
+    loader_images, loader_masks = data
+    print(loader_images.shape, loader_masks.shape)
