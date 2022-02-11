47be7c40a2815393e58aa90370f5ccd870a285da
Vince <vincivenv@gmail.com>
2022-02-11 13:20:29 +0300

    remove hpu code, update for both models.
    Use hydra cli args to switch to different models

--- src/train.py
+++ src/train.py
@@ -8,7 +8,7 @@ import wandb
 from pytorch_lightning.loggers import WandbLogger
 
 from data import ClassifierDataModule, UNETDataModule
-from model import Model, UNETModel
+from model import CLSModel, UNETModel
 
 
 class SamplesVisualisationLogger(pl.Callback):
@@ -42,50 +42,14 @@ class SamplesVisualisationLogger(pl.Callback):
         )
 
 
-def permute_params(model, to_filters_last, lazy_mode):
-    with torch.no_grad():
-        for name, param in model.named_parameters():
-            if param.ndim == 4:
-                if to_filters_last:
-                    param.data = param.data.permute(
-                        (2, 3, 1, 0)
-                    )  # permute KCRS to RSCK
-                else:
-                    param.data = param.data.permute(
-                        (3, 2, 0, 1)
-                    )  # permute RSCK to KCRS
-    if lazy_mode:
-        import habana_frameworks.torch.core as htcore
-
-        htcore.mark_step()
-
-
-def permute_momentum(optimizer, to_filters_last, lazy_mode):
-    # Permute the momentum buffer before using for checkpoint
-    for group in optimizer.param_groups:
-        for p in group["params"]:
-            param_state = optimizer.state[p]
-            if "momentum_buffer" in param_state:
-                buf = param_state["momentum_buffer"]
-                if buf.ndim == 4:
-                    if to_filters_last:
-                        buf = buf.permute((2, 3, 1, 0))
-                    else:
-                        buf = buf.permute((3, 2, 0, 1))
-                    param_state["momentum_buffer"] = buf
-
-    if lazy_mode:
-        import habana_frameworks.torch.core as htcore
-
-        htcore.mark_step()
-
-
 @hydra.main(config_path="configs", config_name="config")
 def main(cfg):
-    data: pl.LightningDataModule = ClassifierDataModule(config=cfg)
-    model = Model(num_classes=cfg.model.num_classes)
-    unet = UNETModel(lr=cfg.model.lr)
-    unet_data = UNETDataModule()
+    cls_data = ClassifierDataModule(config=cfg)
+    cls_model = CLSModel(num_classes=cfg.model.num_classes)
+
+    unet_data = UNETDataModule(config=cfg)
+    unet_model = UNETModel(lr=cfg.model.lr)
+
     check_point = pl.callbacks.ModelCheckpoint(
         dirpath="checkpoints",
         filename="model_checkpoint_{epoch}",
@@ -101,66 +65,42 @@ def main(cfg):
     wandb_logger = WandbLogger(
         project="dl4tb", save_dir=hydra.utils.get_original_cwd() + "/saved/"
     )
-    if cfg.training.device == "hpu":
-        try:
-            from habana_frameworks.torch.utils.library_loader import (
-                load_habana_module,
-            )  # type: ignore
 
-            load_habana_module()  # type: ignore
-            import habana_frameworks.torch.core  # type: ignore
-            import habana_frameworks.torch.core.hccl  # type: ignore
-
-            num_instances = cfg.training.num_instances
-            parallel_hpus = [torch.device("hpu")] * num_instances
-            hpus = True
-            device = torch.device("hpu")
-            import habana_frameworks.torch.core as htcore
-
-            htcore.mark_step()
-
-        except ModuleNotFoundError:
-            device = torch.device("cpu")
-            hpus = False
-    trainer = pl.Trainer(
-        gpus=1,
-        max_epochs=20,
-        fast_dev_run=False,
+    unet_trainer = pl.Trainer(
+        logger=wandb_logger,
+        callbacks=[check_point],
         default_root_dir=cfg.training.save_dir,
-        deterministic=cfg.training.deterministic,
+        tpu_cores=cfg.training.cores if cfg.training.device == "tpu" else 0,
+        gpus=1 if cfg.training.device == "gpu" else 0,
+        fast_dev_run=False,
         limit_train_batches=cfg.training.limit_train_batches,
         limit_val_batches=cfg.training.limit_val_batches,
+        max_epochs=cfg.training.max_epochs,
         log_every_n_steps=1,
-        callbacks=[check_point],
+        deterministic=cfg.training.deterministic,
     )
 
-    trainer.fit(unet, unet_data)
-    # trainer = pl.Trainer(
-    #     # hpus=num_instances if hpus else None,
-    #     gpus=(1 if torch.cuda.is_available() else 0),
-    #     # strategy=pl.plugins.DDPPlugin(
-    #     #     parallel_devices=parallel_hpus,
-    #     #     bucket_cap_mb=cfg.training.bucket_cap_mb,
-    #     #     gradient_as_bucket_view=True,
-    #     #     static_graph=True,
-    #     # )
-    #     # if num_instances > 1
-    #     # else None,
-    #     log_every_n_steps=cfg.training.log_every_n_steps,
-    #     default_root_dir=cfg.training.save_dir,
-    #     max_epochs=cfg.training.max_epochs,
-    #     fast_dev_run=False,
-    #     logger=wandb_logger,
-    #     callbacks=[
-    #         check_point,
-    #         early_stopping_callback,
-    #         SamplesVisualisationLogger(data),
-    #     ],
-    #     deterministic=cfg.training.deterministic,
-    #     limit_train_batches=cfg.training.limit_train_batches,
-    #     limit_val_batches=cfg.training.limit_val_batches,
-    # )
-    # trainer.fit(model, data)
+    cls_trainer = pl.Trainer(
+        logger=wandb_logger,
+        callbacks=[
+            check_point,
+            early_stopping_callback,
+            SamplesVisualisationLogger(cls_data),
+        ],
+        default_root_dir=cfg.training.save_dir,
+        tpu_cores=cfg.training.cores if cfg.training.device == "tpu" else 0,
+        gpus=1 if cfg.training.device == "gpu" else 0,
+        fast_dev_run=False,
+        limit_train_batches=cfg.training.limit_train_batches,
+        limit_val_batches=cfg.training.limit_val_batches,
+        max_epochs=cfg.training.max_epochs,
+        log_every_n_steps=cfg.training.log_every_n_steps,
+        deterministic=cfg.training.deterministic,
+    )
+    if cfg.training.model == "unet":
+        unet_trainer.fit(unet_model, unet_data)
+    else:
+        cls_trainer.fit(cls_model, cls_data)
 
 
 if __name__ == "__main__":
